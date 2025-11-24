95922841bfa490a72df2177c660121cf3fdfbe46
Khalid Abdallah <kabdallah2022@fau.edu>
2025-11-24 10:18:04 -0500

    Initial Commit

---
+++ Parking/ParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingApp.swift
+//  Parking
+//
+//  Created by Khalid Abdallah on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
